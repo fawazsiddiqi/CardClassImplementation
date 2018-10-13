6e82d51782493ecdb82cc4c0ea08ea6b66c952ab
Fawaz Siddiqi <fawazsidd@fawazs-macbook-pro.local>
2018-10-13 12:33:38 +0400

    Initial Commit

---
+++ MohammadFawazSiddiqi_5465163_Ass2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MohammadFawazSiddiqi_5465163_Ass2
+//
+//  Created by Fawaz Siddiqi on 10/13/18.
+//  Copyright © 2018 Fawaz Siddiqi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
